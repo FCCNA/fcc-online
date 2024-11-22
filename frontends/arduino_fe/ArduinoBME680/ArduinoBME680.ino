ca514f733a7010f9d3155479e73159c31b94d089
fcc <fcc@pcfcc>
2024-11-22 09:07:23 +0100

    added back second temperature sensor

--- frontends/arduino_fe/ArduinoBME680/ArduinoBME680.ino
+++ frontends/arduino_fe/ArduinoBME680/ArduinoBME680.ino
@@ -19,12 +19,10 @@
 #include <Adafruit_Sensor.h>
 #include "Adafruit_BME680.h"
 #include "SHT31.h"
-
-#include <Wire.h>
-#include "rgb_lcd.h"
+//#include "rgb_lcd.h"
 
 const int measurement_ms = 500; //time in ms between sensor read
-const int lcd_ms = 2000;        //time in ms between display update
+//const int lcd_ms = 2000;        //time in ms between display update
 
 Timers timer_measurement; // read sensor
 //Timers timer_lcd;         // change LCD screen
@@ -41,32 +39,32 @@ float temperature_sht;
 float humidity_sht;
 
 // page to show on LCD
-int lcd_page = 0;
+//int lcd_page = 0;
 
 void setup() {
   Serial.begin(115200);
   if (!bme.begin(0x76)) {
     Serial.println(F("Could not find a valid BME680 sensor, check wiring!"));
-    while (1);
-  }
+  } else {
 
-  // Set up oversampling and filter initialization
-  bme.setTemperatureOversampling(BME68X_OS_16X);
-  bme.setHumidityOversampling(BME68X_OS_16X);
-  bme.setPressureOversampling(BME68X_OS_16X);
-  bme.setIIRFilterSize(BME68X_FILTER_SIZE_3);
-  bme.setGasHeater(0, 0); // Disable Gas Measurement
+    // Set up oversampling and filter initialization
+    bme.setTemperatureOversampling(BME68X_OS_16X);
+    bme.setHumidityOversampling(BME68X_OS_16X);
+    bme.setPressureOversampling(BME68X_OS_16X);
+    bme.setIIRFilterSize(BME68X_FILTER_SIZE_3);
+    bme.setGasHeater(0, 0); // Disable Gas Measurement
+  }
 
   //begin sht
   sht.begin();
 
   //Start timers
   timer_measurement.start(measurement_ms);
-  //timer_lcd.start(lcd_ms);
+//  timer_lcd.start(lcd_ms);
 
   //start LCD
-  //lcd.begin(16, 2);
-  //lcd.noBlinkLED();
+ // lcd.begin(16, 2);
+ // lcd.noBlinkLED();
 
   //set internal led to mark conversion time
   pinMode(LED_BUILTIN, OUTPUT);
@@ -79,10 +77,10 @@ void loop() {
     timer_measurement.stop();
     //trigger a BME680 read
     unsigned long endTime = bme.beginReading();
-    if (endTime == 0) {
-      Serial.println(F("Failed to begin reading :("));
-      return;
-    }
+    //if (endTime == 0) {
+    //  Serial.println(F("Failed to begin reading :("));
+    //  return;
+    //}
     sht.requestData();
 
     digitalWrite(LED_BUILTIN, HIGH);
@@ -102,7 +100,7 @@ void loop() {
     bool success  = sht.readData(); 
     if (success == false)
     {
-      Serial.println("SHT Failed read");
+      //Serial.println("SHT Failed read");
     }
     else
     {
@@ -129,14 +127,17 @@ void loop() {
       Serial.print(humidity_sht);
       Serial.println(F(" %"));
     }
+    if(val == 'H'){
+      Serial.println(F("Hello"));
+    }
   }
 
   //write to LCD
-  /*  if (timer_lcd.available()) {
+/*  if (timer_lcd.available()) {
       timer_lcd.stop();
       // calculate next page
       lcd_page += 1;
-      if(lcd_page > 2) lcd_page = 0;
+      if(lcd_page > 1) lcd_page = 0;
 
       //write to LCD
       lcd.clear();
@@ -145,26 +146,26 @@ void loop() {
         lcd.setRGB(25, 0, 0);
         lcd.print(F("Temperature"));
         lcd.setCursor(0, 1);
-        lcd.print(temperature);
+        lcd.print(temperature_sht);
         lcd.print(F(" *C"));
       }
       if(lcd_page == 1){
         lcd.setRGB(0, 0, 25);
         lcd.print(F("Humidity"));
         lcd.setCursor(0, 1);
-        lcd.print(humidity);
+        lcd.print(humidity_sht);
         lcd.print(F(" %"));
       }
-      if(lcd_page == 2){
-        lcd.setRGB(0, 25, 0);
-        lcd.print(F("Pressure"));
-        lcd.setCursor(0, 1);
-        lcd.print(pressure);
-        lcd.print(F(" hPa"));
-      }
+      //if(lcd_page == 2){
+      //  lcd.setRGB(0, 25, 0);
+      //  lcd.print(F("Pressure"));
+      //  lcd.setCursor(0, 1);
+      //  lcd.print(pressure_bme);
+      //  lcd.print(F(" hPa"));
+      //}
       timer_lcd.start(lcd_ms);
-  }
-  */
+  }*/
+  
 
   // some idle
   delay(100);
